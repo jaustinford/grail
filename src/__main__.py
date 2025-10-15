74b8c73303f98d1140ccbcd4e5cd8f7ab2288fb6
james austin <j.austin.ford@gmail.com>
2025-10-15 17:17:17 -0600

    wildcards

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,7 @@
 """
+Copy all files and directories in
+/src-backup-path to /dst-backup-path.
 """
-
 import os
 import re
 import json
@@ -22,8 +23,76 @@ LARGE_BINARY_EXTS = [
     "mp4", "mpg", "wav", "webm"
 ]
 
+def clone_dir(src_dir: str, dst_dir: str):
+    """
+    Copy directories and preserve
+    metadata elements.
+    """
+
+    dst_relative = re.sub("/dst-backup-path/", "", dst_dir)
+
+    if not os.path.isdir(dst_dir):
+        LOGGER.info("Creating dir : %s", dst_relative)
+
+        if os.environ.get("BACKUP_MODE") == "copy":
+            os.makedirs(dst_dir)
+            shutil.copystat(
+                src_dir,
+                dst_dir,
+                follow_symlinks=False
+            )
+
+    else:
+        LOGGER.info("Confirmed dir : %s", dst_relative)
+
+def clone_file(src_file: str, dst_file: str):
+    """
+    Copy files and preserve
+    metadata elements.
+    """
+
+    should_copy = False
+
+    dst_relative = re.sub("/dst-backup-path/", "", dst_file)
+
+    if not os.path.isfile(dst_file):
+        LOGGER.info("Creating file : %s", dst_relative)
+        should_copy = True
+
+    else:
+        should_compare = True
+
+        for large_binary_ext in LARGE_BINARY_EXTS:
+            if src_file.endswith("." + large_binary_ext):
+                should_compare = False
+
+        if should_compare:
+            if not filecmp.cmp(src_file, dst_file):
+                LOGGER.info("Updating standard file : %s", dst_relative)
+                should_copy = True
+
+            else:
+                LOGGER.info("Confirmed standard file : %s", dst_relative)
+
+        else:
+            if os.path.getsize(src_file) != os.path.getsize(dst_file):
+                LOGGER.info("Updating large binary file : %s", dst_relative)
+                should_copy = True
+
+            else:
+                LOGGER.info("Confirmed large binary file : %s", dst_relative)
+
+    if should_copy and os.environ.get("BACKUP_MODE") == "copy":
+        shutil.copy2(
+            src_file,
+            dst_file,
+            follow_symlinks=False
+        )
+
 def resolve_wildcards(src_fqdn: str):
     """
+    Return a list of directories
+    fitting a wildcard pattern.
     """
 
     matched_files = []
@@ -44,75 +113,15 @@ def resolve_wildcards(src_fqdn: str):
 
     return matched_files
 
-def clone_dir(backup_name: str, src_dir: str, dst_dir: str):
+def resolve_dirs(src_dir: str):
     """
-    """
-
-    dst_relative = re.sub("/dst-backup-path/", "", dst_dir)
-
-    if not os.path.isdir(dst_dir):
-        LOGGER.info("( " + backup_name.upper() + " ) Creating dir : %s", dst_relative)
-
-        if os.environ.get("BACKUP_MODE") == "copy":
-            os.makedirs(dst_dir)
-            shutil.copystat(
-                src_dir,
-                dst_dir,
-                follow_symlinks=False
-            )
-
-    else:
-        LOGGER.info("( " + backup_name.upper() + " ) Confirmed dir : %s", dst_relative)
-
-def clone_file(backup_name: str, src_file: str, dst_file: str):
-    """
-    """
-
-    should_copy = False
-
-    dst_relative = re.sub("/dst-backup-path/", "", dst_file)
-
-    if not os.path.isfile(dst_file):
-        LOGGER.info("( " + backup_name.upper() + " ) Creating file : %s", dst_relative)
-        should_copy = True
-
-    else:
-        should_compare = True
-
-        for large_binary_ext in LARGE_BINARY_EXTS:
-            if src_file.endswith("." + large_binary_ext):
-                should_compare = False
-
-        if should_compare:
-            if not filecmp.cmp(src_file, dst_file):
-                LOGGER.info("( " + backup_name.upper() + " ) Updating standard file : %s", dst_relative)
-                should_copy = True
-
-            else:
-                LOGGER.info("( " + backup_name.upper() + " ) Confirmed standard file : %s", dst_relative)
-
-        else:
-            if os.path.getsize(src_file) != os.path.getsize(dst_file):
-                LOGGER.info("( " + backup_name.upper() + " ) Updating large binary file : %s", dst_relative)
-                should_copy = True
-
-            else:
-                LOGGER.info("( " + backup_name.upper() + " ) Confirmed large binary file : %s", dst_relative)
-
-    if should_copy and os.environ.get("BACKUP_MODE") == "copy":
-        shutil.copy2(
-            src_file,
-            dst_file,
-            follow_symlinks=False
-        )
-
-def walk_dir(backup_name: str, src_dir: str):
-    """
+    Walk directories and process
+    files separate from nested
+    directories.
     """
 
     for walk_roots, walk_dirs, walk_files in os.walk(src_dir):
         clone_dir(
-            backup_name,
             walk_roots,
             re.sub("/src-backup-path/", "/dst-backup-path/", walk_roots)
         )
@@ -121,16 +130,18 @@ def walk_dir(backup_name: str, src_dir: str):
             dir_src_fqdn = os.path.join(walk_roots, walk_dir)
             dir_dst_fqdn = re.sub("/src-backup-path/", "/dst-backup-path/", dir_src_fqdn)
 
-            clone_dir(backup_name, dir_src_fqdn, dir_dst_fqdn)
+            clone_dir(dir_src_fqdn, dir_dst_fqdn)
 
         for walk_file in walk_files:
             file_src_fqdn = os.path.join(walk_roots, walk_file)
             file_dst_fqdn = re.sub("/src-backup-path/", "/dst-backup-path/", file_src_fqdn)
 
-            clone_file(backup_name, file_src_fqdn, file_dst_fqdn)
+            clone_file(file_src_fqdn, file_dst_fqdn)
 
-def process_backup(backup_name: str, backup_target: str):
+def process_backup(backup_target: str):
     """
+    Direct the flow along whether
+    'backup_target_src_fqdn' has a wildcard.
     """
 
     backup_target_src_fqdn = "/src-backup-path/" + backup_target
@@ -150,16 +161,17 @@ def process_backup(backup_name: str, backup_target: str):
     for backup_target_fqdn in backup_target_fqdns:
         if os.path.isfile(backup_target_fqdn[0]):
             clone_file(
-                backup_name,
                 backup_target_fqdn[0],
                 backup_target_fqdn[1]
             )
 
         elif os.path.isdir(backup_target_fqdn[0]):
-            walk_dir(backup_name, backup_target_fqdn[0])
+            resolve_dirs(backup_target_fqdn[0])
 
 def main():
     """
+    Open config and assign elements and
+    iterate over 'backup_targets'.
     """
 
     with open(BACKUPS_FILE, "r", encoding="utf-8") as backups_opened:
@@ -167,20 +179,17 @@ def main():
         backups_json = json.loads(backups_read)
 
     for backup_json in backups_json["backups"]:
-        backup_name = backup_json["name"]
-
-        if backup_name == os.environ.get("BACKUP_OBJECT"):
+        if backup_json["name"] == os.environ.get("BACKUP_OBJECT"):
             backup_targets = backup_json["targets"]
             break
 
     if os.environ.get("BACKUP_MODE") == "check":
-        LOGGER.info("( " + backup_name.upper() + " ) Running in check mode.")
+        LOGGER.info("Running in check mode.")
+
+    LOGGER.info("Processing backup : %s", backup_json["name"])
 
     for backup_target in backup_targets:
-        process_backup(
-            backup_name,
-            backup_target
-        )
+        process_backup(backup_target)
 
 if __name__ == "__main__":
     main()
